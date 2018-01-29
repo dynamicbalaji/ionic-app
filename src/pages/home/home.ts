7cfc1cc43fb08cea32a74793ae1ab6abcceb201b
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2018-01-29 01:04:29 -0600

    Updated home page and added related images

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+export interface Actions {
+  action: string;
+  icon: string;
+}
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+
+  allActions: Actions[] = [
+    {action:'Clocked Hours', icon:'clock.png'},
+    {action:'Schedule for next 7 days', icon:'schedule.png'},
+    {action:'Punches Missed', icon:'schedule.png'},
+    {action:'Available Shifts', icon:'shifts.jpg'},
+    {action:'Pay Stubs', icon:'payslip.png'}
+  ];
 
   constructor(public navCtrl: NavController) {
 
