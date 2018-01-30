b9391b82c433b7804c6b147085dcd530b988782f
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2018-01-30 11:12:10 -0600

    Added code changes for success page

--- src/pages/change-schedule-done/change-schedule-done.ts
+++ src/pages/change-schedule-done/change-schedule-done.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 /**
  * Generated class for the ChangeScheduleDonePage page.
@@ -8,7 +8,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
 @Component({
   selector: 'page-change-schedule-done',
   templateUrl: 'change-schedule-done.html',
