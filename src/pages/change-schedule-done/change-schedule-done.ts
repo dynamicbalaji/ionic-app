0c4cf2054d0476dc26493aa47af1b4c939eda9d0
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2018-01-30 15:01:57 -0600

    Code changes for hiding and displaying success msgs

--- src/pages/change-schedule-done/change-schedule-done.ts
+++ src/pages/change-schedule-done/change-schedule-done.ts
@@ -19,6 +19,25 @@ export class ChangeScheduleDonePage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ChangeScheduleDonePage');
+    document.getElementById("one").className = "spinnerCenter";
+    document.getElementById("one").className += "my-label";
+    document.getElementById("one").style.margin = "0 auto";
+    document.getElementById("two").style.display = "none";
+    document.getElementById("three").style.display = "none";
+    setTimeout(function () {  
+        document.getElementById("one").style.display = "none";
+        document.getElementById("two").style.display = "inline";
+        document.getElementById("two").className = "spinnerCenter";
+        document.getElementById("two").className += "my-label";
+        document.getElementById("two").style.margin = "0 auto";
+    }, 6000); 
+    setTimeout(function () {  
+        document.getElementById("two").style.display = "none"; 
+        document.getElementById("three").style.display = "inline";
+        document.getElementById("three").className = "spinnerCenter"; 
+        document.getElementById("three").className += "my-label";
+        document.getElementById("three").style.margin = "0 auto";
+    }, 12000);
   }
 
 }
