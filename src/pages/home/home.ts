aea2ee56b5fcb61a2ceff733dfd17951f4f7ff76
BK <Balaji_Kumar03@ad.infosys.com>
2018-01-31 16:30:18 -0600

    Modified images to ionicons in home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,10 @@ import { ChangeSchedulePage } from '../change-schedule/change-schedule';
 export interface Actions {
   action: string;
   icon: string;
+  title1?: string;
+  title2?: string;
+  subTitle1?: string;
+  subTitle2?: string;
 }
 
 @Component({
@@ -15,11 +19,11 @@ export interface Actions {
 export class HomePage {
 
   allActions: Actions[] = [
-    {action:'Clocked Hours', icon:'clock.png'},
-    {action:'Schedule for next 7 days', icon:'schedule.png'},
-    {action:'Punches Missed', icon:'schedule.png'},
-    {action:'Available Shifts', icon:'shifts.jpg'},
-    {action:'Pay Stubs', icon:'payslip.png'}
+    {action:'Clocked Hours', icon:'clock', title1:'22 hours', subTitle1: '15th-31th Jan 2018'},
+    {action:'Schedule for next 7 days', icon:'calendar', title1:'Today', subTitle1: '7am-4pm', title2:'Tomorrow', subTitle2: '11am-8pm'},
+    {action:'Punches Missed', icon:'card', title1:'2 days'},
+    {action:'Available Shifts', icon:'alarm', title1:'15th Jan', subTitle1: '7am-4pm', title2:'16th Jan', subTitle2: '11am-8pm'},
+    {action:'Pay Stubs', icon:'cash'}
   ];
 
   constructor(public navCtrl: NavController) {
