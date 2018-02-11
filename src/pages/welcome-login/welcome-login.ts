6cfaff0323556bf00d4cff8ec5b380486280f412
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-02-11 17:34:01 -0600

    Implemented login service for login functionality

--- src/pages/welcome-login/welcome-login.ts
+++ src/pages/welcome-login/welcome-login.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { VoiceEnablePage } from '../voice-enable/voice-enable';
+import { AssociateService } from '../../services/associate.service';
 
 /** 
  * Generated class for the WelcomeLoginPage page.
@@ -15,19 +18,40 @@ import { VoiceEnablePage } from '../voice-enable/voice-enable';
 })
 export class WelcomeLoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  loginForm: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public formBuilder: FormBuilder, public ascService: AssociateService) {
+    this.loginForm = formBuilder.group({
+      username: ['', Validators.compose([Validators.required, Validators.pattern('[a-zA-Z]*'), Validators.maxLength(10)])],
+      password: ['', Validators.compose([Validators.required])]
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad WelcomeLoginPage');
-    setTimeout(function () {  
+    setTimeout(function () {
       document.getElementById("greeting").style.display = "none";
       document.getElementById("login").style.display = "inline";
-  }, 6000); 
+    }, 6000);
   }
 
-  NextPage() {
+  login() {
     this.navCtrl.push(VoiceEnablePage);
     this.navCtrl.setRoot(VoiceEnablePage);
+  }
+
+  onSubmit(value: any): void {
+    if (this.loginForm.valid) {
+      this.ascService.getLoginInfo(value.username, value.password).then(data => {
+        console.log(data);
+        if (data.validUser) {
+          console.log("murugan");
+          this.navCtrl.push(VoiceEnablePage);
+          this.navCtrl.setRoot(VoiceEnablePage);
+        }
+      }
+      );
     }
+  }
 }
