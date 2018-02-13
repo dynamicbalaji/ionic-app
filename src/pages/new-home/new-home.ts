c6eaaadb15b73c2faf5deac8b64ce8de7449b43f
dynamicbalaji <balajiashokkumar@yahoo.com>
2018-02-13 14:33:15 -0600

    Added service calls for home page

--- src/pages/new-home/new-home.ts
+++ src/pages/new-home/new-home.ts
@@ -6,7 +6,7 @@ import { PunchMissedPage } from '../punch-missed/punch-missed';
 import * as moment from 'moment';
 
 import { LocalNotifications } from '@ionic-native/local-notifications';
-import { Geolocation ,GeolocationOptions ,Geoposition ,PositionError } from '@ionic-native/geolocation';
+import { Geolocation, GeolocationOptions, Geoposition, PositionError } from '@ionic-native/geolocation';
 //import { GeoLocationServiceProvider } from '../../providers/geo-location-service/geo-location-service';
 import { DashboardService } from '../../services/dashboard.service';
 import { ScheduleNew } from '../../app/interfaces';
@@ -26,7 +26,7 @@ export class NewHomePage {
   notifications: any[] = [];
   currentPosition: Geoposition;
   options: GeolocationOptions;
-  lat = 36.365025; 
+  lat = 36.365025;
   lon = -94.214577;
   clockedHours: string = "0";
   punchesMissedCount: string = "0";
@@ -40,23 +40,26 @@ export class NewHomePage {
   previousCurrentPayPeriod1: string = "";
   previousCurrentPayPeriod2: string = "";
   speakerEnabled = 'true';
-  points:any = "50";
+  points: any = "50";
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform,
-              public alertCtrl: AlertController, public localNotifications: LocalNotifications, 
-              public geolocation: Geolocation, public dashboardService: DashboardService) {
-      
+    public alertCtrl: AlertController, public localNotifications: LocalNotifications,
+    public geolocation: Geolocation, public dashboardService: DashboardService) {
+
     this.addNotification(2, 'You have entered the Walmart store. Please login and start your shift.');
     this.addNotification(5, 'Your Meal break is coming up in 5 minutes, kindly take the needed break. Enjoy your meal! ');
     this.addNotification(16, 'You have not yet ended your shift.'); // Delayed to show crossed time
 
-    console.log('Default Notification time - from HOME: '+ moment(new Date()).format());
-
+    console.log('Default Notification time - from HOME: ' + moment(new Date()).format());
+    this.populateAllDetails();
+    this.todayDate = moment().format('ll');
+    this.tomorrowDate = moment().add(1, 'days').format('ll');
+    this.pastMonthDate = moment().subtract(45, 'days').format('ll');
     this.scheduleNotifications();
     this.getAssocPosition();
   }
 
-  populateAllDetails(){
+  populateAllDetails() {
     this.dashboardService.fetchTimeAndAttendanceInfo().then(data => {
       let a = JSON.stringify(data);
       let b = JSON.parse(a);
@@ -93,67 +96,67 @@ export class NewHomePage {
     console.log('ionViewDidLoad NewHomePage');
   }
 
-  viewSchedule(){
+  viewSchedule() {
     this.navCtrl.push(WeeklySchedulePage);
   }
 
 
-  viewPunchMissed(){
+  viewPunchMissed() {
     this.navCtrl.push(PunchMissedPage);
   }
 
 
-  addNotification(notifyid: number, msg: string){
+  addNotification(notifyid: number, msg: string) {
 
     let notification = {
-        id: notifyid,
-        title: 'Hi, Emily!',
-        text: msg,
-        at: new Date().setMinutes((new Date().getMinutes() + notifyid),0,0)
+      id: notifyid,
+      title: 'Hi, Emily!',
+      text: msg,
+      at: new Date().setMinutes((new Date().getMinutes() + notifyid), 0, 0)
     };
 
     this.notifications.push(notification);
     console.log("Notification added to array: ", notification);
   }
 
-  scheduleNotifications(){  
- 
-      if(this.platform.is('cordova')){
-          // Cancel any existing notifications
-          this.localNotifications.cancelAll().then(() => {
-  
-              // Schedule the new notifications
-              this.localNotifications.schedule(this.notifications);
-              console.log("Notifications are scheduled: "+ this.notifications);
-              this.notifications = [];
-  
-              /* let alert = this.alertCtrl.create({
-                  title: 'Notifications setup success!',
-                  buttons: ['OK']
-              }); */
-  
-              //alert.present(); 
-          });
-  
-    }else{
+  scheduleNotifications() {
+
+    if (this.platform.is('cordova')) {
+      // Cancel any existing notifications
+      this.localNotifications.cancelAll().then(() => {
+
+        // Schedule the new notifications
+        this.localNotifications.schedule(this.notifications);
+        console.log("Notifications are scheduled: " + this.notifications);
+        this.notifications = [];
+
+        /* let alert = this.alertCtrl.create({
+            title: 'Notifications setup success!',
+            buttons: ['OK']
+        }); */
+
+        //alert.present(); 
+      });
+
+    } else {
       //console.log("Not a Cordova platform: "+ this.notifications);
-            // let alert = this.alertCtrl.create({
-            //     title: 'Notifications not set !',
-            //     buttons: ['OK']
-            // });
- 
-            // alert.present(); 
+      // let alert = this.alertCtrl.create({
+      //     title: 'Notifications not set !',
+      //     buttons: ['OK']
+      // });
+
+      // alert.present(); 
     }
   }
- 
-  cancelAll(){
+
+  cancelAll() {
     this.localNotifications.cancelAll();
- 
+
     /* let alert = this.alertCtrl.create({
         title: 'Notifications cancelled',
         buttons: ['Ok']
     }); */
- 
+
     //alert.present();
   }
 
@@ -188,49 +191,49 @@ export class NewHomePage {
   //      });
   //      alert1.present();
   //  }
-  
-   getAssocPosition(){
-      this.options = {
-        enableHighAccuracy : true
-      };
-      this.geolocation.getCurrentPosition(this.options).then(
-        (position : Geoposition) => {
-          this.currentPosition = position;    
-          console.log(position.coords);
-          this.calcPosition(position.coords.latitude,position.coords.longitude,this.lat,this.lon);
 
-      },(err : PositionError)=>{
-          console.log("error : " + err.message);
+  getAssocPosition() {
+    this.options = {
+      enableHighAccuracy: true
+    };
+    this.geolocation.getCurrentPosition(this.options).then(
+      (position: Geoposition) => {
+        this.currentPosition = position;
+        console.log(position.coords);
+        this.calcPosition(position.coords.latitude, position.coords.longitude, this.lat, this.lon);
+
+      }, (err: PositionError) => {
+        console.log("error : " + err.message);
       });
 
-   }
+  }
 
-   calcPosition(lat1,lon1,lat2,lon2): boolean{
-      var result: boolean = false;
-      var R = 3958.75; // radius in mi
-      var x1 = lat2-lat1;
-      var dLat = this.toRad(x1);  
-      var x2 = lon2-lon1;
-      var dLon = this.toRad(x2); 
-      var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
-              Math.cos(this.toRad(lat1)) * Math.cos(this.toRad(lat2)) * 
-              Math.sin(dLon/2) * Math.sin(dLon/2);  
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-      var d = R * c; 
-      console.log("You are"+" "+d+" "+"miles away from gpc");
-      var dispresult = "You are currently at Latitude: "+lat1+" Longitude: "+ lon1 +" and you are "+d+" "+"miles away from Store " + " at Latitude: "+ lat2 + " Longitude: " + lon2;
-      //var time = new Date();
-      if(d <= 0.5){
-          result = true;
-          this.addNotification(0, 'You have entered the Walmart store. Please login and start your shift.');
-      }
-      console.log("Your Result: "+ dispresult + " "+ result);
-
-      return result;
-   }
-
-    toRad(coord1) {
-      return coord1 * Math.PI / 180;
+  calcPosition(lat1, lon1, lat2, lon2): boolean {
+    var result: boolean = false;
+    var R = 3958.75; // radius in mi
+    var x1 = lat2 - lat1;
+    var dLat = this.toRad(x1);
+    var x2 = lon2 - lon1;
+    var dLon = this.toRad(x2);
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(this.toRad(lat1)) * Math.cos(this.toRad(lat2)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    console.log("You are" + " " + d + " " + "miles away from gpc");
+    var dispresult = "You are currently at Latitude: " + lat1 + " Longitude: " + lon1 + " and you are " + d + " " + "miles away from Store " + " at Latitude: " + lat2 + " Longitude: " + lon2;
+    //var time = new Date();
+    if (d <= 0.5) {
+      result = true;
+      this.addNotification(0, 'You have entered the Walmart store. Please login and start your shift.');
     }
+    console.log("Your Result: " + dispresult + " " + result);
+
+    return result;
+  }
+
+  toRad(coord1) {
+    return coord1 * Math.PI / 180;
+  }
 
 }
